1abcc70a9aca2883b9ccc62fcdd8b4a446f22aa0
Marina Guseva <marina.guseva@contentful.com>
2019-11-18 17:31:03 +0100

    Add validations for Planet

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,2 +1,6 @@
 class Planet < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true
+  validates :capacity, presence: true
+  # todo check if we handle empty photo
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning the db"
+Planet.destroy_all if Rails.env.development?
 
 # create planets
-
-planets = ["Mars", "Venus", "Saturn"]
+puts "Creating planets..."
+planets = %w[Mahasim Proxima\ Centauri Rigil Kentaurus Luyten\ 726-8A Procyon\ A
+Sun Sirius\ A Betelgeuse Ross\ 154 Vega Luyten\ 726-8B Procyon\ B
+Wolf\ 359 Mars Venus Saturn]
 planets.each do |planet|
   Planet.create!(
     name: planet,
@@ -18,13 +22,4 @@ planets.each do |planet|
     description: Faker::Lorem.sentences(number: 10))
 end
 
-# create stars
-
-20.times {
-  Planet.create!(
-    name: Faker::Space.star,
-    price: rand(100..1000),
-    capacity: rand(1..15),
-    # travel_time: rand(86400..150000),
-    description: Faker::Lorem.sentences(number: 10))
-}
+puts "Done!"
