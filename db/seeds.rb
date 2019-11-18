37692b0eb7e5936866adb1953634a59e6c98b9cb
Marina Guseva <marina.guseva@contentful.com>
2019-11-18 17:00:38 +0100

    Add seed file with planets

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# create planets
+
+planets = ["Mars", "Venus", "Saturn"]
+planets.each do |planet|
+  Planet.create!(
+    name: planet,
+    price: rand(100..1000),
+    capacity: rand(1..15),
+    # travel_time: rand(86400..150000),
+    description: Faker::Lorem.sentences(number: 10))
+end
+
+# create stars
+
+20.times {
+  Planet.create!(
+    name: Faker::Space.star,
+    price: rand(100..1000),
+    capacity: rand(1..15),
+    # travel_time: rand(86400..150000),
+    description: Faker::Lorem.sentences(number: 10))
+}
