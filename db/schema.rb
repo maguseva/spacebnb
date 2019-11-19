8778e014f83858f5df191e95fe0c1e15406ed8b3
moritz gosmann <moritz.gosmann@outlook.de>
2019-11-19 19:07:09 +0100

    latest merged changes b

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_101816) do
+ActiveRecord::Schema.define(version: 2019_11_19_135443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 2019_11_19_101816) do
     t.string "travel_time"
     t.string "description"
     t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "star"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
