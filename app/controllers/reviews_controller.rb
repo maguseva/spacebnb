1778575a3e45aad61eca2db9e72f5af4f1d9124d
Franziska von Bethmann <franziska.bethmann@gmail.com>
2019-11-20 15:58:31 +0100

    fixed index resource

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,8 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
   def new
     @review = Review.new
     @booking = Booking.find(params[:booking_id])
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :planets, only: [:index, :show] do
     resources :bookings, only: [:new, :create]
-      resources :reviews, only: [:new, :create]
+      resources :reviews, only: [:index, :new, :create]
   end
   resources :reviews, only: [:destroy]
   resources :bookings, only: [:index, :destroy]
