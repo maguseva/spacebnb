f5a96e18706a82bb726f5dd34e58a64df3ac6d55
Marina Guseva <marina.guseva@contentful.com>
2019-11-22 15:42:12 +0100

    Double fix or no fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import { priceCalculator } from "./price_calculator";
 
-import '../node_modules/flatpickr/dist/flatpickr.min.css';
+import '../../../node_modules/flatpickr/dist/flatpickr.min.css';
 
 initMapbox();
 priceCalculator();
